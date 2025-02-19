ba20d1e7cb8ccd030814c0b49286a86b7fb91e43
Matheus <matheusleidow@gmail.com>
2025-02-19 20:44:27 -0300

    React Portal e Error Boundary feitos

--- src/Components/Error.tsx
+++ src/Components/Error.tsx
@@ -1,13 +1,31 @@
+import React from 'react'
 import ReactDom from 'react-dom'
 
-const ErrorMessage = () => {
-    return ReactDom.createPortal(
-        <div className='error-message'>
-            <h1>Ocorreu um Erro.</h1>
-        </div>,
-        document.getElementById('error')!
-    )
+interface IProps {
+    children: React.ReactNode
 }
 
-export default ErrorMessage
+class ErrorBoundary extends React.Component<IProps> {
+    state = { hasError: false }
 
+    static getDerivedStateFromError() {
+        return { hasError: true }
+    }
+
+    componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+        console.error('Erro Identificado na Aplicação: ', error, errorInfo)
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return ReactDom.createPortal(
+                <h1>Ocorreu um Problema</h1>,
+                document.getElementById('error') as HTMLElement
+            )
+        }
+        return this.props.children
+    }
+}
+
+
+export default ErrorBoundary
